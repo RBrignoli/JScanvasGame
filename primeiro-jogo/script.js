eb985bda5098640dda8ef8870ba410e7c5292bbb
Professor <Professor@SN-392529.edu.fiesc.com.br>
2025-02-20 16:31:44 -0300

    alteracoes aula

--- primeiro-jogo/script.js
+++ primeiro-jogo/script.js
@@ -1,24 +1,45 @@
 const canvas = document.getElementById('jogoCanvas')
 const ctx = canvas.getContext('2d')
+let gravidade = 0.5
 
+let gameOver = false
 document.addEventListener("click", (e) => {
     if (gameOver==true){
         location.reload()
     }
 })
 
-let gameOver = false
+document.addEventListener('keypress', (e) =>{
+    if (e.code == 'Space' && personagem.pulando == false){
+        personagem.velocidadey = -15
+        personagem.pulando = true
+    }
+})
+
 
 const personagem = {
     posicaox:50,
     posicaoy: canvas.height -50,
     largura: 50,
-    altura: 50
+    altura: 50,
+    velocidadey: 0,
+    pulando: false
 }
 function desenhaPersonagem () {
     ctx.fillStyle = 'white'
     ctx.fillRect(personagem.posicaox, personagem.posicaoy, personagem.largura, personagem.altura)
 }
+function atualizaPersonagem (){
+    if(personagem.pulando == true){
+        personagem.velocidadey += gravidade
+        personagem.posicaoy += personagem.velocidadey
+        if(personagem.posicaoy>=canvas.height-50){
+            personagem.velocidadey = 0
+            personagem.pulando=false
+        }
+    }
+}
+
 const obstaculo = {
     posicaoXObstaculo: canvas.width-100,
     posicaoYObstaculo: canvas.height - 100,
@@ -38,12 +59,21 @@ function desenhaObstaculo (){
 
 function atualizaObstaculo(){
     obstaculo.posicaoXObstaculo -= obstaculo.velocidade
+    if (obstaculo.posicaoXObstaculo <= 0-obstaculo.largura){
+        obstaculo.posicaoXObstaculo = canvas.width-100
+        let altura_random = (Math.random() * 50)+90
+        obstaculo.altura = altura_random
+        obstaculo.posicaoYObstaculo = canvas.height - altura_random
+        obstaculo.velocidade += 0.5
+    }
 }
 
 function verificaColisao(){
     if(
-        personagem.posicaox - 1 < obstaculo.posicaoXObstaculo + obstaculo.largura &&
-        personagem.posicaox + personagem.largura > obstaculo.posicaoXObstaculo
+        personagem.posicaox < obstaculo.posicaoXObstaculo + obstaculo.largura &&
+        personagem.posicaox + personagem.largura > obstaculo.posicaoXObstaculo &&
+        personagem.posicaoy < obstaculo.posicaoYObstaculo + obstaculo.altura &&
+        personagem.posicaoy + personagem.altura > obstaculo.posicaoYObstaculo
     ){  
         houveColisao()
     }
@@ -66,6 +96,7 @@ function loop () {
     desenhaObstaculo()
     desenhaPersonagem()
     atualizaObstaculo()
+    atualizaPersonagem()
     verificaColisao()
  
     requestAnimationFrame(loop)
