389202fe265dd572c075ae51886dac661ed85d96
Professor <Professor@SN-392529.edu.fiesc.com.br>
2025-02-25 16:43:51 -0300

    initiate classes

---
+++ primeiro-jogo/script2.js
@@ -0,0 +1,44 @@
+class Entidade {
+    #gravidade
+    constructor(x,y,largura,altura){
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura=altura
+        this.#gravidade=0.5
+    }
+    desenhar = function (ctx, cor) {
+        ctx.fillStyle = cor
+        ctx.fillRect(this.x, this.y, this.largura, this.altura)
+    }
+    atualizar = function(){
+        //modificar esta entidade para atualizar posição do objeto na tela
+        console.log('atualizar posiçao da entidade na tela')
+    }
+}
+class Personagem extends Entidade{
+    #velocidadey
+    constructor(x,y,largura,altura){
+        super(x,y,largura,altura);
+        this.#velocidadey=0
+        this.pulando = false
+    }
+    saltar = function(){
+        console.log('saltar')
+    }
+    atualizar = function(){
+        console.log('atualizar personagem na tela')
+    }
+
+}
+class Obstaculo extends Entidade{
+    #velocidadex
+    constructor(x,y,largura,altura){
+        super(x,y,largura,altura);
+        this.#velocidadex=100
+    }
+    atualizar = function(){
+        console.log('atualizar obstaculo na tela')
+    }
+}
+
